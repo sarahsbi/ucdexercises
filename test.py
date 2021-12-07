ab17233c7f09f89b92c75047874da2633170ced0
sarahsbi <92694879+sarahsbi@users.noreply.github.com>
2021-12-07 23:41:23 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-netflix_data= pd.read_csv("netflix_titles.csv")
-
-drop_duplicates= netflix_data.drop_duplicates(subset=['type'])
-print(netflix_data.shape,drop_duplicates.shape)
-
